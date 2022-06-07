f3c5f28c4c6fc8bd3764b55487444ee3e9dbf018
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-06-07 14:42:49 +0200

    Add task 1 - Evaluate the last digit of a number

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point: Evaluates the last digit of a number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int last;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n < 0)
+		last = ((n * -1) % 10) * -1;
+	else
+		last = n % 10;
+
+	if (last > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+	else if (last == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last);
+	else if (last < 6)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+
+	return (0);
+}
