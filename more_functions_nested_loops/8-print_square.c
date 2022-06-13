6bf581068bd58089b8613ab459ecacb58c94d0d5
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-06-13 13:03:27 +0200

    Add task 8 - Print a square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square
+ * @size: The square's size
+ */
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		int i;
+		int j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
