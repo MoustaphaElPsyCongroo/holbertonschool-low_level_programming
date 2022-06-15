3f6493d5ea0559523361bdce98607e13a49d75de
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-06-15 09:55:17 +0200

    Add task 7 - Print last half of a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string
+ * @str: The string's half to print
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int length;
+
+	while (str[i])
+		i++;
+
+	length = i;
+
+	if (length % 2 != 0)
+		i = (i - 1) / 2;
+	else
+		i /= 2;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
