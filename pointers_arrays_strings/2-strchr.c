8519b3779e89f63ba93175f8d47575d5e850e9eb
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-06-19 19:08:46 +0200

    Add task 2 - Locates a character in a string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: The string to search in
+ * @c: The character to search in s
+ *
+ * Return: A pointer to the first occurence of c in s
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s && *s != c)
+		s++;
+
+	if (*s == 0)
+		return (0);
+
+	return (s);
+}
