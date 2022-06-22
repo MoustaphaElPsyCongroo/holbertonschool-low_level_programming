72807aa1e7c06b73c133f75986e0b8f384ede8cf
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-06-22 12:38:26 +0200

    Add task 0 - Make a program print its own name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints this program's name
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0
+ */
+int main(__attribute__((unused)) int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
