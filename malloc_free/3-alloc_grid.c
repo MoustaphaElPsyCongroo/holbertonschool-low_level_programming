abdbb24ada0379f128b9d20c0e18dee2055e3ba4
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-06-23 05:20:54 -0700

    Add task 3 - Create a dynamic two dimensional array of integers

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * alloc_grid - Initializes a 2 dimensional array of zeroes
+ * @width: The number of sub arrays
+ * @height: The number of outer arrays
+ *
+ * Return:
+ *	A pointer to the array
+ *	NULL on failure or if width or height are 0 or negative
+ */
+int **alloc_grid(int width, int height)
+{
+	int i;
+	int j;
+	int **a;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	a = malloc(width * sizeof(int *));
+
+	for (i = 0; i < height; i++)
+		a[i] = malloc(width * sizeof(int));
+
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			a[i][j] = 0;
+		}
+	}
+
+	return (a);
+}
