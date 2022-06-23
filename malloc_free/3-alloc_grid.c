3c7ba1d62316d288b2e75e97966b3317e57475b0
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-06-23 05:38:09 -0700

    Try correct memory allocation

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -20,7 +20,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	a = malloc(width * sizeof(int *));
+	a = malloc(sizeof(int) * width);
 
 	for (i = 0; i < height; i++)
 	{
