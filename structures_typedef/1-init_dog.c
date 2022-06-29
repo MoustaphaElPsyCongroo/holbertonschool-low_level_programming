a628525785ea87ff6f3e2ea716ffe57982018a18
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-06-29 20:02:19 +0200

    Add task 1 - Initialize a variable of type struct dog

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - Initializes a variable of type struct dog
+ * @d: A pointer to the structure
+ * @name: The name element of dog struct
+ * @age: The age element of dog struct
+ * @owner: The owner element of dog struct
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef dog_h
 #define dog_h
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 /**
  * struct dog - A dog
  * @name: Dog's name
