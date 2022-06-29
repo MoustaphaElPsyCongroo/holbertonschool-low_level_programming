5dda753f190aa7d22b42aeb21582101a150c67c4
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-06-29 19:52:21 +0200

    Add task 0 - Define a new struct

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - A dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
