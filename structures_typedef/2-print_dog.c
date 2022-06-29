1166ef759a010fccd5fdb69c5cdf61519d84f142
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-06-29 20:27:13 +0200

    Add task 2 - Print a struct dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - Prints a struct dog
+ * @d: A pointer to a struct dog
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", d->name != NULL ? d->name : "nil");
+
+		if (d->age < 0)
+			printf("Age: %s\n", "nil");
+		else
+			printf("Age: %.5f\n", d->age);
+
+		printf("Owner: %s\n", d->owner != NULL ? d->owner : "nil");
+	}
+}
