cf1f43fc77347b8032f547b130b676747909e2e5
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-07-12 14:42:24 +0200

    Add memory freeing in case of error

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -18,7 +18,10 @@ list_t *add_node(list_t **head, const char *str)
 	new_head = malloc(sizeof(list_t));
 
 	if (new_head == NULL)
+	{
+		free(new_head);
 		return (NULL);
+	}
 
 	while (s[length])
 		length++;
