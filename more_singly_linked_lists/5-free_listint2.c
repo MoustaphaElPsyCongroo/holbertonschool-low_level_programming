b130f3357fbd76785789084cc094457bf574eeb2
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-07-13 13:32:36 +0200

    Add task 5 - Free a linked list

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a listint_t linked list, setting the head to NULL
+ * @head: Head of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tofree;
+
+	while (*head != NULL)
+	{
+		tofree = (*head)->next;
+		free(*head);
+		*head = tofree;
+	}
+}
