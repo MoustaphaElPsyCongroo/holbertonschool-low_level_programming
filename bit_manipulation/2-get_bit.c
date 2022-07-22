9237ec97b9a6f51bc138eb05cd2864778aad96ba
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-07-22 22:41:39 +0200

    Add task 2 - Get the value of a bit at a given index

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * get_bit - Gets the value of a bit at a given index
+ * @n: The number to examine
+ * @index: Index of the bit to retrieve
+ *
+ * Return: The value of the bit at index index, or -1 if error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int j = 0;
+	unsigned long int k = 1;
+	unsigned int digits;
+	int bit_val = -1;
+
+	while (k << j <= n)
+	{
+		if (j == 64)
+			break;
+
+		j++;
+	}
+
+	if (j > 0)
+		j--;
+
+	digits = j + 1;
+
+	while (digits > index)
+	{
+		unsigned long int bit = k << j;
+
+		if (bit <= n)
+		{
+			bit_val = 1;
+			n -= bit;
+		}
+		else
+		{
+			bit_val = 0;
+		}
+
+		j--;
+		digits--;
+	}
+
+	return (bit_val);
+}
