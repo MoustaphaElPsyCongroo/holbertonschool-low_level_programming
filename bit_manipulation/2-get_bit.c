6719e0a7a186b83e128ff8a084ea95e558289d63
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-07-22 23:20:43 +0200

    Add task 2 - Return the value of a bit at given index

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -9,44 +9,8 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int j = 0;
-	unsigned long int k = 1;
-	unsigned int digits;
-	int bit_val = -1;
+	if (index > 64)
+		return (-1);
 
-	while (k << j <= n)
-	{
-		if (j == 64)
-			break;
-
-		j++;
-	}
-
-	if (j > 0)
-		j--;
-
-	digits = j + 1;
-
-	if (index > digits)
-		return (0);
-
-	while (digits > index)
-	{
-		unsigned long int bit = k << j;
-
-		if (bit <= n)
-		{
-			bit_val = 1;
-			n -= bit;
-		}
-		else
-		{
-			bit_val = 0;
-		}
-
-		j--;
-		digits--;
-	}
-
-	return (bit_val);
+	return ((n >> index) & 1);
 }
