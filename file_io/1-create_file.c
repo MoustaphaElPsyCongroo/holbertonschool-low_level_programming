7ef71212e6d10d020537e6f723b961aa18248fe6
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-07-24 03:23:09 +0200

    Correct task 1 - Case of null filename and forgot to close the file descriptor

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -21,9 +21,12 @@ int create_file(const char *filename, char *text_content)
 	int length;
 	ssize_t written;
 
+	if (filename == NULL)
+		return (-1);
+
 	length = strlen(text_content);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0500);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
@@ -35,6 +38,8 @@ int create_file(const char *filename, char *text_content)
 		if (written == -1)
 			return (-1);
 	}
+
+	close(fd);
 
 	return (1);
 }
