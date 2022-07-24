1a5b367f1f882186130427f6912d9361b524c24d
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-07-25 00:00:44 +0200

    Correct 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -20,7 +20,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
-		*head->prev = NULL;
+		(*head)->prev = NULL;
 		free(looper);
 		return (1);
 	}
