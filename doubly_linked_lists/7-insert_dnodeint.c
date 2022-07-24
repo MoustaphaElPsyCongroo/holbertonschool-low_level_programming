c56cf7c82316007cfe801c2046de10a1efb49494
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-07-24 23:45:37 +0200

    try correct 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,19 +15,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *looper = *h;
 	dlistint_t *new_node;
 
-	new_node = malloc(sizeof(dlistint_t));
-
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
-
 	if (idx == 0 || *h == NULL)
 	{
-		new_node->next = *h;
-		*h = new_node;
+		new_node = add_dnodeint(h, n);
 	}
 	else
 	{
@@ -35,19 +25,26 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			if (looper->next == NULL && idx - i != 1)
 			{
-				free(new_node);
 				return (NULL);
+			}
+			else if (looper->next == NULL)
+			{
+				new_node = add_dnodeint_end(h, n);
+				return (new_node);
 			}
 
 			looper = looper->next;
 			i++;
 		}
 
+		new_node = malloc(sizeof(dlistint_t));
+
+		if (new_node == NULL)
+			return (NULL);
+
 		new_node->next = looper->next;
 		new_node->prev = looper;
-
-		if (looper->next != NULL)
-			looper->next->prev = new_node;
+		looper->next->prev = new_node;
 		looper->next = new_node;
 	}
 	return (new_node);
