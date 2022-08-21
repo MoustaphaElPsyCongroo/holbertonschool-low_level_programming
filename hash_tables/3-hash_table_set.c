64badaf827c099f78cf3c52c84b41babbc2d6413
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-08-21 18:36:40 +0200

    Correct 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,52 +1,11 @@
 #include "hash_tables.h"
 #include <string.h>
 #include <stdlib.h>
-
-hash_node_t *add_node_beginning(hash_node_t **, char *, const char *);
-
-/**
- * add_node_beginning - Adds a new node at the end of a linked list
- * @head: A linked list
- * @key: THe key of the new node
- * @value: The content of the new node
- *
- * Return: The address of the new element, or NULL if fail
- */
-hash_node_t *add_node_beginning(hash_node_t **head, char *key, const char *value)
-{
-	hash_node_t *new_head;
-	char *s = strdup(value);
-	int length = 0;
-
-	if (s == NULL)
-	{
-		free(s);
-		return (NULL);
-	}
-
-	new_head = malloc(sizeof(hash_node_t));
-
-	if (new_head == NULL)
-	{
-		free(s);
-		free(new_head);
-		return (NULL);
-	}
-
-	while (s[length])
-		length++;
-
-	new_head->key = key;
-	new_head->value = s;
-	new_head->next = *head;
-	*head = new_head;
-
-	return (new_head);
-}
+#include <stdio.h>
 
 /**
  * hash_table_set - Adds an element to the hash table
- * @ht: The hash table
+ * @ht: The hash table where to add or update the key/value
  * @key: The key to add
  * @value: The value associated to the key
  *
@@ -55,18 +14,43 @@ hash_node_t *add_node_beginning(hash_node_t **head, char *key, const char *value
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *head;
-	hash_node_t *success;
+	hash_node_t *head, *looper;
 
 	if (!ht || !key || key[0] == 0 || !value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
+	looper = ht->array[index];
 
-	head = ht->array[index];
-	success = add_node_beginning(&head, (char *)key, value);
-
-	if (!success)
+	while (looper)
+	{
+		if (strcmp(looper->key, key) == 0)
+		{
+			free(looper->value);
+			looper->value = strdup(value);
+			if (!looper->value)
+				return (0);
+			return (1);
+		}
+		looper = looper->next;
+	}
+	head = malloc(sizeof(hash_node_t));
+	if (!head)
 		return (0);
+	head->key = strdup(key);
+	if (!head->key)
+	{
+		free(head);
+		return (0);
+	}
+	head->value = strdup(value);
+	if (!head->value)
+	{
+		free(head);
+		free(head->key);
+		return (0);
+	}
+	head->next = ht->array[index];
+	ht->array[index] = head;
 	return (1);
 }
