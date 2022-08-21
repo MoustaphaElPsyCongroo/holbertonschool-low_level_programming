bfa801a6658b632d706adaad4d44f722ad477b4a
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-08-21 18:47:49 +0200

    Add task 4 - Retrieve a value associated with a key

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with a key
+ * @ht: The hash table to look into
+ * @key: The key to search
+ *
+ * Return: The value associated with the key, or NULL if key not found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *looper;
+
+	if (!key || strcmp(key, "") == 0 || !ht)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	looper = ht->array[index];
+
+	while (looper)
+	{
+		if (strcmp(looper->key, key) == 0)
+			return (looper->value);
+		looper = looper->next;
+	}
+
+	return (NULL);
+}
